c2e88a14d6cd31f16614ab4d07b702d8c72efde2
Lautaro261 <nahuelygoofy@gmail.com>
2023-03-27 13:57:30 -0300

    base de datos planteada

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,43 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    id:{                                          // ID
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
     },
+    name: {                                       //NOMBRE
+      type: DataTypes.STRING,
+      allowNull: false,   // ¿Permito que sea nulo? NOO
+    },
+    image:{                                      //IMAGEN
+        type:DataTypes.STRING,
+        allowNull: true,   // ¿Permito que sea nulo? SI, puede ser
+    },
+    hp:{                                       //VIDA
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    attack:{                                    //ATAQUE
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    defense:{                                   //DEFENSA
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    speed:{                                     //VELOCIDAD
+        type: DataTypes.STRING,
+        allowNull:true, 
+    },
+    height:{                                    //ALTURA
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    weight:{                                    //PESO
+        type: DataTypes.STRING,
+        allowNull:true,
+    }
+
   });
 };
