4879d26c006a8f41c6d8ff6dd8bcffa5d87bbe26
Lautaro261 <nahuelygoofy@gmail.com>
2023-03-28 00:59:47 -0300

    funcion getAPIPokemons anda joya! :D

--- api/src/controllers/GET/getPokemons.js
+++ api/src/controllers/GET/getPokemons.js
@@ -1,7 +1,7 @@
 const { Pokemon } = require('../../db');
 const axios = require('axios')
 const URL = 'https://pokeapi.co/api/v2/pokemon';
-const limit = '?limit=5'
+const limit = '?limit=2'
 
 
 
@@ -20,29 +20,39 @@ const getDataBasePokemons = async (req, res)=>{
 const getAPIPokemons = async(req, res)=>{
     try {
         const responseAPI = await axios.get(`${URL}${limit}`)
-        const arrayPeticiones = responseAPI.data?.results.map((char)=>{
-            return axios.get(char.url)
-        }) 
+        const array1results = responseAPI.data.results  // es una array 
+        //console.log(`ESTO ES array1results ${array1results}`)
+
+         const arrayPeticiones = array1results.map((char)=>{
+           // console.log(char.url)
+            return axios.get(char.url)}) 
+            //console.log(`ESTO ES arrayPeticiones ${arrayPeticiones}`) 
+
         const pokes = await axios.all(arrayPeticiones)
-        console.log(arrayPeticiones)
 
-        
+        let datas = pokes?.map((x)=>{
+            //console.log(x.data.name)
+            return x.data})  
+
+        const pokemons = datas.map((pokemon)=>{
+            return {
+                id: pokemon.id,
+                name: pokemon.name,
+                image: pokemon.sprites.front_default,
+                hp: pokemon.stats[0].base_stat,
+                attack: pokemon.stats[1].base_stat,
+                defense: pokemon.stats[2].base_stat,
+                speed: pokemon.stats[5].base_stat,
+                height: pokemon.height,
+                weight: pokemon.weight,
+            }
+        })
 
 
-        res.status(200).json(arrayPeticiones)
+        res.status(200).json(pokemons)
 
 
 
-
-        /*const apiAllPokes = await axios.get(`${API_URL_POKES}?limit=40`);   
-         console.log('apiAllPokes')
-        console.log(apiAllPokes) 
-        const apiPokes = apiAllPokes.data?.results.map(e => axios.get(e.url)); //ingreso a todos los urls
-         console.log('apiPokes')
-        console.log(apiPokes)   
-        const pokesUrlInfo = await axios.all(apiPokes) //espero a que se cumplan todas las promises 
-         console.log('pokesUrlInfo')
-        console.log(pokesUrlInfo.length)   */
 
 
 
